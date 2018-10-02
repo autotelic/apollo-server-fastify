e6432523add148087c22064e9729fd05b976ad98
Aditya Pratap Singh <adisinghrajput@gmail.com>
2018-10-03 01:59:56 +0200

    feat(fastify): Integrate apollo-fastify plugin #626

--- packages/apollo-server-cache-memcached/src/__tests__/Memcached.test.ts
+++
@@ -1,7 +0,0 @@
-// use mock implementations for underlying databases
-jest.mock('memcached', () => require('memcached-mock'));
-
-import { MemcachedCache } from '../index';
-import { testKeyValueCache } from '../../../apollo-server-caching/src/__tests__/testsuite';
-
-testKeyValueCache(new MemcachedCache('localhost'));
